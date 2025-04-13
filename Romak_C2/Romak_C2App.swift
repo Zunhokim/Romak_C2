7e92310910fbdb9ff8f340b1b8a3989a30045623
zunobono <zxcrtf245@naver.com>
2025-04-13 14:52:14 +0900

    Initial Commit

---
+++ Romak_C2/Romak_C2App.swift
@@ -0,0 +1,17 @@
+//
+//  Romak_C2App.swift
+//  Romak_C2
+//
+//  Created by 준호 on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Romak_C2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Romak_C2Tests/Romak_C2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Romak_C2Tests.swift
+//  Romak_C2Tests
+//
+//  Created by 준호 on 4/13/25.
+//
+
+import Testing
+@testable import Romak_C2
+
+struct Romak_C2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
